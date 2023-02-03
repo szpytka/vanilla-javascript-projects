293635ba4abf71ac94201efe40ed7fdf34f32e82
szpytka <jakub.szpytka@gmail.com>
2023-02-03 21:04:32 +0100

    added two new projects - scroll animation and split landing page

---
+++ 8. Scroll Animation/script.js
@@ -0,0 +1,17 @@
+const boxes = document.querySelectorAll('.box');
+
+checkBoxes();
+
+function checkBoxes() {
+  const triggerBottom = (window.innerHeight / 5) * 4;
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+    if (boxTop < triggerBottom) {
+      box.classList.add('show');
+    } else {
+      box.classList.remove('show');
+    }
+  });
+}
+
+window.addEventListener('scroll', checkBoxes);
