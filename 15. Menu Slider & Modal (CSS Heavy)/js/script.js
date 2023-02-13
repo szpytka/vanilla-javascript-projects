d0e9dfddfbb20a4058d7197f53fd20351fe091ea
szpytka <jakub.szpytka@gmail.com>
2023-02-13 20:09:03 +0100

    new project!

---
+++ 15. Menu Slider & Modal (CSS Heavy)/js/script.js
@@ -0,0 +1,24 @@
+const toggle = document.getElementById('toggle');
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
+
+// Toggle Nav
+toggle.addEventListener('click', () => {
+  document.body.classList.toggle('show-nav');
+});
+
+// Show Modal
+open.addEventListener('click', () => {
+  modal.classList.add('show-modal');
+});
+
+// Hide Modal
+close.addEventListener('click', () => {
+  modal.classList.remove('show-modal');
+});
+
+// Hide Modal On Outside Click
+window.addEventListener('click', (e) =>
+  e.target == modal ? modal.classList.remove('show-modal') : false
+);
