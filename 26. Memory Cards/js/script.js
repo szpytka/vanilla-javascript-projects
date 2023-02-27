408214501d4f83e871652baa7e2063a260edbf60
szpytka <jakub.szpytka@gmail.com>
2023-02-27 09:49:12 +0100

    Memory Cards project
    add, remove, localstorage

---
+++ 26. Memory Cards/js/script.js
@@ -0,0 +1,146 @@
+const cardsContainer = document.getElementById('cards-container');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const currentEl = document.getElementById('current');
+const showBtn = document.getElementById('show');
+const hideBtn = document.getElementById('hide');
+const questionEl = document.getElementById('question');
+const answerEl = document.getElementById('answer');
+const addCardBtn = document.getElementById('add-card');
+const clearBtn = document.getElementById('clear');
+const addContainer = document.getElementById('add-container');
+
+// Keep track of current card
+let currentActiveCard = 0;
+
+// Store DOM cards
+const cardsEl = [];
+
+// Store card data
+const cardsData = getCardsData();
+// const cardsData = [
+//   {
+//     question: 'What must a variable begin with?',
+//     answer: 'A letter, $ or _',
+//   },
+//   {
+//     question: 'What is a variable?',
+//     answer: 'Container for a piece of data',
+//   },
+//   {
+//     question: 'Example of Case Sensitive Variable',
+//     answer: 'thisIsAVariable',
+//   },
+// ];
+
+// Create all cards
+function createCards() {
+  cardsData.forEach((data, index) => createCard(data, index));
+}
+
+// Create a single card in DOM
+function createCard(data, index) {
+  const card = document.createElement('div');
+  card.classList.add('card');
+  if (index === 0) {
+    card.classList.add('active');
+  }
+  card.innerHTML = `
+    <div class="inner-card">
+          <div class="inner-card-front">
+            <p>${data.question}</p>
+          </div>
+          <div class="inner-card-back">
+            <p>${data.answer}</p>
+          </div>
+        </div>
+  `;
+
+  card.addEventListener('click', () => {
+    card.classList.toggle('show-answer');
+  });
+
+  // add to dom cards
+  cardsEl.push(card);
+  cardsContainer.appendChild(card);
+  updateCurrentText();
+}
+
+// show number of cards
+function updateCurrentText() {
+  currentEl.innerText = `${currentActiveCard + 1}/${cardsEl.length}`;
+}
+
+// get cards from local storage
+function getCardsData() {
+  const cards = JSON.parse(localStorage.getItem('cards'));
+  return cards === null ? [] : cards;
+}
+
+// add card to local storage
+function setCardsData(cards) {
+  localStorage.setItem('cards', JSON.stringify(cards));
+  window.location.reload();
+}
+
+createCards();
+
+// event listeners
+
+// next button
+nextBtn.addEventListener('click', () => {
+  cardsEl[currentActiveCard].className = 'card left';
+  currentActiveCard = currentActiveCard + 1;
+  if (currentActiveCard > cardsEl.length - 1) {
+    currentActiveCard = cardsEl.length - 1;
+  }
+  cardsEl[currentActiveCard].className = 'card active';
+  updateCurrentText();
+});
+
+// prev button
+prevBtn.addEventListener('click', () => {
+  cardsEl[currentActiveCard].className = 'card right';
+  currentActiveCard = currentActiveCard - 1;
+  if (currentActiveCard < 0) {
+    currentActiveCard = 0;
+  }
+  cardsEl[currentActiveCard].className = 'card active';
+  updateCurrentText();
+});
+
+// show add new card
+showBtn.addEventListener('click', () => {
+  addContainer.classList.add('show');
+});
+
+// hide add new card
+hideBtn.addEventListener('click', () => {
+  addContainer.classList.remove('show');
+});
+
+// add new card
+addCardBtn.addEventListener('click', () => {
+  const question = questionEl.value;
+  const answer = answerEl.value;
+
+  if (question.trim() && answer.trim()) {
+    const newCard = {
+      question: question,
+      answer: answer,
+    };
+    createCard(newCard);
+    questionEl.value = '';
+    answerEl.value = '';
+    addContainer.classList.remove('show');
+    cardsData.push(newCard);
+    setCardsData(cardsData);
+  }
+});
+
+// Clear cards button
+clearBtn.addEventListener('click', () => {
+  localStorage.clear();
+  cardsContainer.innerHTML = '';
+  window.location.reload();
+});
